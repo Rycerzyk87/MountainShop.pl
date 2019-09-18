236d3bffcae55e7a40a1ee64bbd9fd5f2a26f75d
Adam Samulski <a.samulski@op.pl>
2019-09-18 21:26:03 +0200

    header router

--- shop/src/modules/Header.js
+++ shop/src/modules/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import '../style/Header.css';
 import header1 from '../images/mountainfog.jpg';
@@ -11,17 +11,39 @@ import header5 from '../images/mountaivillage.jpg';
 
 function Header() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/" exact render={() => (<img src={header1} alt="mountains with fog" />)} />
-                <Route path="/ofert" render={() => (<img src={header2} alt="mountains and lake" />)} />
-                <Route path="/opinions" render={() => (<img src={header3} alt="mountain and fog2" />)} />
-                <Route path="/client" render={() => (<img src={header4} alt="rocks" />)} />
-                <Route path="/contact" render={() => (<img src={header5} alt="village in mountains" />)} />
-                {/* na dole route do error 404 */}
-                <Route render={() => (<img src={header3} alt="mountain and fog2" />)} />
-            </Switch>
-        </Router >
+
+        <BrowserRouter>
+            <>
+                <Switch>
+                    <Route path="/" render={() => (
+                        <div className='header'>
+                            <img src={header1} alt="mountains with fog" />
+                        </div>)} />
+                    <Route path="/ofert" render={() => (
+                        <div className='header'>
+                            <img src={header2} alt="mountains and lake" />
+                        </div>)} />
+                    <Route path="/opinions" render={() => (
+                        <div className='header'>
+                            <img src={header3} alt="mountain and fog2" />
+                        </div>
+                    )} />
+                    <Route path="/client" render={() => (
+                        <div className='header'>
+                            <img src={header4} alt="rocks" />
+                        </div>)} />
+                    <Route path="/contact" render={() => (
+                        <div className='header'>
+                            <img src={header5} alt="village in mountains" />
+                        </div>)} />
+                    {/* na dole route do error 404 */}
+                    <Route render={() => (
+                        <div className='header'>
+                            <img src={header3} alt="mountain and fog2" />
+                        </div>)} />
+                </Switch>
+            </>
+        </BrowserRouter >
 
     );
 }
