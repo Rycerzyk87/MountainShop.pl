fe107f362301db3e4a451866ea62ab3915524da4
Adam Samulski <a.samulski@op.pl>
2019-09-19 20:39:11 +0200

    fetch city

--- shop/src/pages/Offer.js
+++ shop/src/pages/Offer.js
@@ -1,11 +1,53 @@
 import React from 'react';
 
+const cityList = ['zakopane', "ustroń", "wisła", "krynica-zdrój"];
+const cityOptions = cityList.map(city => (
+    <option>{city}</option>
+))
 
+class Offer extends React.Component {
+    state = {
+        city: "",
+        weather: [],
+        key: "99c6651300a41971c7dd60f651d33589",
+    }
+    // cityOptions = () => {
+    //     cityList.map(item => (
+    //         <option>{item}</option>
+    //     ))
+    // }
+    getWeather = () => {
 
-const Offer = () => {
-    return (
-        <div className="offer">Offer</div>
-    );
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        const targetUrl = `api.openweathermap.org/data/2.5/weather?q=${this.state.city}&APPID=${this.state.key}`;
+
+        fetch(proxyUrl + targetUrl, {
+            method: 'GET',
+        }).then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    data: data
+                })
+
+            }
+            );
+    };
+
+    componentDidMount() {
+        this.getWeather();
+    }
+    render() {
+
+        return (
+            <div>
+                <select>
+                    {cityOptions}
+                </select>
+            </div>
+        );
+    }
 }
 
 export default Offer;
+
