48839afbff7267c7199ed95d6b459a97a558704e
Adam Samulski <a.samulski@op.pl>
2019-10-05 21:12:55 +0200

    contact.js validation

--- shop/src/pages/Contact.js
+++ shop/src/pages/Contact.js
@@ -8,15 +8,15 @@ class Contact extends React.Component {
         value: "",
         valueMail: "",
 
-        // errors: {
-        //     value: true,
-        //     valueMail: true
-        // }
+        errors: {
+            value: false,
+            valueMail: false
+        }
     }
-    // errorsMessages = {
-    //     value_incorrect: "Wiadomość musi zawierać przynajmniej 15 znaków",
-    //     valueMail_incorrect: "Adres mailowy musi zawierać znak '@'",
-    // }
+    errorsMessages = {
+        value_incorrect: "Wiadomość musi zawierać przynajmniej 15 znaków",
+        valueMail_incorrect: "Adres mailowy musi zawierać znak '@'",
+    }
 
     handleChange = (e) => {
         this.setState({
@@ -30,10 +30,39 @@ class Contact extends React.Component {
     }
 
     handleSubmit = (e) => {
-        this.setState({
-            value: "",
-            valueMail: ""
+        e.preventDefault();
+        const validation = this.formValidation();
+        if (validation.correct) {
+            this.setState({
+                value: "",
+                valueMail: "",
 
+                errors: {
+                    value: false,
+                    valueMail: false
+                }
+            })
+        } else {
+            this.setState({
+                errors: {
+                    value: !validation.value,
+                    valueMail: !validation.valueMail
+                }
+            })
+        }
+    }
+    formValidation = () => {
+        let value = false;
+        let valueMail = false;
+        let correct = false;
+
+        if (this.state.value.length > 15 && this.state.value.indexOf(' ') === -1) { value = true }
+        if (this.state.valueMail.indexOf('@') !== -1) { valueMail = true }
+        if (value && valueMail) { correct = true }
+        return ({
+            value,
+            valueMail,
+            correct,
         })
     }
     render() {
@@ -44,11 +73,11 @@ class Contact extends React.Component {
                         <form onSubmit={this.handleSubmit}>
                             <label htmlFor="valueMail">
                                 <h3>Napisz do nas!</h3>
-                                {/* {this.state.errors.valueMail && <p className="error">{this.errorsMessages.valueMail_incorrect}</p>} */}
+                                {this.state.errors.valueMail && <p className="error">{this.errorsMessages.valueMail_incorrect}</p>}
                                 <input value={this.state.valueMail} onChange={this.handleChangeMail} placeholder="Twój adres mailowy"></input>
                             </label>
                             <label htmlFor="value">
-                                {/* {this.state.errors.value && <p className="error">{this.errorsMessages.value_incorrect}</p>} */}
+                                {this.state.errors.value && <p className="error">{this.errorsMessages.value_incorrect}</p>}
                                 <textarea value={this.state.value} onChange={this.handleChange} placeholder="Zostaw nam wiadomość"></textarea><br></br>
                             </label>
                             <button onSubmit={this.handleSubmit}>Wyślij</button>
@@ -61,7 +90,7 @@ class Contact extends React.Component {
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.642785143959!2d18.853076488426414!3d50.44317588188764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47112a2eef652e85%3A0xdffc2e861b102af2!2sZUS%20w%20Tarnowskich%20G%C3%B3rach!5e0!3m2!1spl!2spl!4v1570038597024!5m2!1spl!2spl" style={{ width: "40vw", height: "50vh", frameborder: "0", }} title="elbrus"></iframe></div>
                     </div>
                     <div className="tele">
-                        <p><i class="fa fa-phone"> </i> 700 72 772</p>
+                        <p><i className="fa fa-phone"> </i> 700 72 772</p>
                     </div>
                 </div>
 
